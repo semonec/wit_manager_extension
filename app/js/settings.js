eb233f917fa0cb2b5639a295c13e1406bd3c7e00
semonec <semonec@gmail.com>
2018-12-13 08:45:20 +0900

    설정페이지 추가 구현 - 개발자에게 쪽지 보내기 까지

---
+++ app/js/settings.js
@@ -0,0 +1,7 @@
+$("#wit-btn-send-msg-to-dev").click(function(){ 
+  const new_win = window.open(
+    "http://witland.xyz/bbs/memo_form.php?me_recv_mb_id=google_a07f09e3",
+    'win_memo',
+    'left=100,top=100,width=620,height=500,scrollbars=1');
+  new_win.focus();
+});
