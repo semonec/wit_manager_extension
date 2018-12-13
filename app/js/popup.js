8dd6ea6ab4e09d354cd8af3ea053b2609752abfc
semonec <semonec@gmail.com>
2018-12-13 12:06:42 +0900

    팝업 잠시 disable - 클릭시 위트랜드 보내는게 더 좋아보임.. 설정가는건 뭐.. 페이지에 인젝션으로 가자

--- app/js/popup.js
+++ app/js/popup.js
@@ -1,3 +1,6 @@
 $(document).on('DOMContentLoaded', () => {
   $("#pressme").click(() => chrome.runtime.openOptionsPage());
+  $('#go').click(() => chrome.tabs.create({ 
+    url: 'http://witland.xyz'
+  }))
 });
