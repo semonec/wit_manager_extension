375c48d997d0bd9386c08b625b5241631c240a28
semonec <semonec@gmail.com>
2018-12-13 09:34:56 +0900

    add gitignore, move images and 128sized icon added

--- app/js/settings.js
+++ app/js/settings.js
@@ -1,3 +1,4 @@
+// send message to developer
 $("#wit-btn-send-msg-to-dev").click(function(){ 
   const new_win = window.open(
     "http://witland.xyz/bbs/memo_form.php?me_recv_mb_id=google_a07f09e3",
