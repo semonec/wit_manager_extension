beaf603459685b43080088694b25daac772485e5
semonec <semonec@gmail.com>
2018-12-13 15:25:37 +0900

    위트랜드 내 접속 시 활성화 스크립트 코드 수정 - 쓰기상태여부 체크 루틴 추가

---
+++ app/js/runner.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+  const currentUrl = window.location.href;
+  if (currentUrl.match('witland.xyz/bbs/write.php')) {
+    // in case of write article
+    // write plugin should used in here
+  } else {
+    // none write case
+  }
+});
